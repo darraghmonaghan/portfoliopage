40485778a634a3ef691bab87995065d1060c5f89
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-04-03 17:18:48 +0100

    added HubSpot tracking code

---
+++ public/javascript/4Calendar.rb
@@ -0,0 +1,17 @@
+
+full_array = [[0,1],[3,5],[4,8],[10,12],[9,10]]
+
+
+def condense_meeting_times(array) 
+	condensed_array = []
+	current_end = 0;
+
+
+	array.each do | slot |
+
+		puts slot[0]
+	end
+end
+
+
+condense_meeting_times(full_array)
